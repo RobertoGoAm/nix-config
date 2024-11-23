98a8b8971479677f94050bb88339e94c20c219a2
Roberto Gomez <robertogoam@disroot.org>
2024-11-23 19:29:54 +0100

    feat: initial commit

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
-# nix-config
+# RobertoGoAm Nix Config
+
+Multiplatform nix configuration to handle installing applications, configuring them and setting up the system for MacOS and Linux-based systems.
+
+## MacOS steps
+
+Install dependencies
+
+```bash
+xcode-select --install
+```
+
+Install the Nix package manager
+
+```bash
+curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix | sh -s -- install
+```
+
+Download from the repo without git
+
+```bash
+curl -L https://github.com/robertogoam/nix-config/archive/refs/heads/main.zip -o - | unzip > repository.zip
+```
+
+Go to the `nix` folder and run:
+
+```bash
+nix run nix-darwin -- switch --flake .
+```
+
+if that doesn't work, it is probably because your hostname does not match with any of the configurations specified in `flake.nix`. If that is the case, use this instead replacing `{configurationName}` with one the available ones in `flake.nix`:
+
+```bash
+nix run nix-darwin -- switch --flake .#{configurationName}
+```
---
+++ flake.nix
@@ -0,0 +1,59 @@
+{
+  description = "RobertoGoAm's nix configuration";
+
+  inputs = {
+    nixpkgs = {
+      url = "github:NixOS/nixpkgs/nixos-unstable";
+    };
+
+    home-manager = {
+      url = "github:nix-community/home-manager/master";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    nix-darwin = {
+      url = "github:LnL7/nix-darwin";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    mac-app-util = {
+      url = "github:hraban/mac-app-util";
+    };
+  };
+
+  outputs = { self, nixpkgs, home-manager, nix-darwin, mac-app-util, ... } @ inputs: {
+    darwinConfigurations = {
+      # Desktop Mac
+      vulcan = nix-darwin.lib.darwinSystem {
+        system = "aarch64-darwin";
+
+        modules = [
+          mac-app-util.darwinModules.default
+          home-manager.darwinModules.home-manager
+          {
+            users.users.robertogoam = {
+              home = "/Users/robertogoam";
+              packages = with nixpkgs; [
+                git
+              ];
+            };
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.robertogoam = {
+              imports = [
+                mac-app-util.homeManagerModules.default
+                ./hosts/vulcan/home.nix
+              ];
+            };
+
+            imports = [
+              ./hosts/vulcan/configuration.nix
+            ];
+
+            _module.args.self = self;
+          }
+        ];
+      };
+    };
+  };
+}
