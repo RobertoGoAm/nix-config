eda82e69450baedc95599b843e41786da8735ddf
Roberto Gomez <robertogoam@disroot.org>
2024-11-24 11:24:57 +0100

    fix: fixed dock config position

--- hosts/vulcan/configuration.nix
+++ hosts/vulcan/configuration.nix
@@ -46,46 +46,46 @@
           InitialKeyRepeat = 25;
         };
       };
-    };
 
-    dock = {
-      # Rearrange Spaces based on most recent use
-      mru-spaces = false;
+      dock = {
+        # Rearrange Spaces based on most recent use
+        mru-spaces = false;
 
-      # Dock position
-      orientation = "left";
+        # Dock position
+        orientation = "left";
 
-      # Permanent apps on dock
-      persistent-apps = [
-        "/System/Library/CoreServices/Finder.app"
-        "/System/Applications/Calendar.app"
-        "/Applications/Safari.app"
-        "/Applications/Chromium.app"
-        "/Applications/Google Chrome.app"
-        "/System/Applications/Mail.app"
-        "/Applications/Spotify.app"
-        "/Applications/Visual Studio Code.app"
-        "/Applications/iTerm.app"
-        "/Applications/Telegram.app"
-      ];
+        # Permanent apps on dock
+        persistent-apps = [
+          "/System/Library/CoreServices/Finder.app"
+          "/System/Applications/Calendar.app"
+          "/Applications/Safari.app"
+          "/Applications/Chromium.app"
+          "/Applications/Google Chrome.app"
+          "/System/Applications/Mail.app"
+          "/Applications/Spotify.app"
+          "/Applications/Visual Studio Code.app"
+          "/Applications/iTerm.app"
+          "/Applications/Telegram.app"
+        ];
 
-      # Permanent folders on dock
-      persistent-others = [
-        "~/Downloads"
-        "~/Development"
-      ];
+        # Permanent folders on dock
+        persistent-others = [
+          "~/Downloads"
+          "~/Development"
+        ];
 
-      # Don't show recent apps
-      show-recents = false;
+        # Don't show recent apps
+        show-recents = false;
 
-      # Icon size
-      tilesize = 42;
+        # Icon size
+        tilesize = 42;
 
-      # Hot corners https://daiderd.com/nix-darwin/manual/index.html#opt-system.defaults.dock.wvous-bl-corner
-      wvous-bl-corner = 1;
-      wvous-br-corner = 1;
-      wvous-tl-corner = 1;
-      wvous-tr-corner = 1;
+        # Hot corners https://daiderd.com/nix-darwin/manual/index.html#opt-system.defaults.dock.wvous-bl-corner
+        wvous-bl-corner = 1;
+        wvous-br-corner = 1;
+        wvous-tl-corner = 1;
+        wvous-tr-corner = 1;
+      };
     };
 
     stateVersion = 5;
