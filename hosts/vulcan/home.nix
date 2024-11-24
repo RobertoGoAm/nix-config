db7caf0b707e496621db7e8e5a6a7e3a89446db4
Roberto Gomez <robertogoam@disroot.org>
2024-11-24 22:15:47 +0100

    feat: added vim and zsh plus configurations

--- hosts/vulcan/home.nix
+++ hosts/vulcan/home.nix
@@ -8,5 +8,21 @@
     home-manager = {
       enable = true;
     };
+
+    vim = {
+      enable = true;
+      defaultEditor = true;
+    };
+
+    zsh = {
+      enable = true;
+      enableBashCompletion = true;
+      enableCompletion = true;
+      enableFastSyntaxHighlighting = true;
+      enableSyntaxHighlighting = true;
+      oh-my-zsh = {
+        enable = true;
+      };
+    };
   };
 }
