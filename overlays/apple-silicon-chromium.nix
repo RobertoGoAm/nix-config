9fe8d89b7c722a429ab6ab3ecb453ac1aa9254f4
Roberto Gomez <robertogoam@disroot.org>
2024-12-01 20:33:37 +0100

    feat: switched to vanilla chromium for macOS

--- overlays/apple-silicon-chromium.nix
+++ overlays/apple-silicon-chromium.nix
@@ -1,25 +1,25 @@
 self: super: {
   chromium = super.stdenv.mkDerivation rec {
-    version = "M126.0.6478.231";
+    version = "1368521";
 
-    name = "Thorium-${version}";
-    buildInputs = [ super.undmg super.unzip ];
+    name = "Chromium-${version}";
+    buildInputs = [ super.unzip ];
     sourceRoot = ".";
     phases = [ "unpackPhase" "installPhase" ];
     installPhase = ''
       mkdir -p "$out/Applications"
-      cp -r Chromium.app "$out/Applications/Chromium.app"
+      cp -r chrome-mac/Chromium.app "$out/Applications/Chromium.app"
     '';
 
     src = super.fetchurl {
-      name = "Thorium_MacOS_ARM64.dmg";
-      url = "https://github.com/Alex313031/Thorium-MacOS/releases/download/${version}/Thorium_MacOS_ARM64.dmg";
-      sha256 = "041aa435b43b42308c7d4f32424891a95de0f62a63728d572b147b5585568628";
+      name = "Mac_Arm_${version}_chrome-mac.zip";
+      url = "https://www.googleapis.com/download/storage/v1/b/chromium-browser-snapshots/o/Mac_Arm%2F${version}%2Fchrome-mac.zip?generation=1728953032008580&alt=media";
+      sha256 = "86ed1b3b90886c3ba0666b8e330681f77fe5240aa49da1375059029e9b58f12b";
     };
 
     meta = with super.stdenv.lib; {
-      description = "Thorium - The fastest browser on Earth.";
-      homepage = "https://thorium.rocks/";
+      description = "Chromium";
+      homepage = "http://www.chromium.org";
       maintainers = with super.maintainers; [ robertogoam ];
       platforms = [ "aarch64-darwin" ];
     };
