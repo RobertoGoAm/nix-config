a5613a0cbef5e9080d716c31964459d27eb01c68
Roberto Gomez <robertogoam@disroot.org>
2024-12-03 18:46:20 +0100

    feat: removed chromium exentsions unrelated to development

--- programs/chromium.nix
+++ programs/chromium.nix
@@ -1,27 +1,17 @@
-{ pkgs, ... }:
+{ ... }:
 {
   programs.chromium = {
     enable = true;
 
-    dictionaries = [
-      pkgs.hunspellDictsChromium.en_US
-    ];
-
     extensions = [
       { id = "nngceckbapebfimnlniiiahkandclblb"; } # Bitwarden
-      { id = "fhcgjolkccmbidfldomjliifgaodjagh"; } # Cookie AutoDelete
       { id = "lfhmikememgdcahcdlaciloancbhjino"; } # CORS Unblock
-      { id = "eimadpbcbfnmbkopoojfekhnkhdbieeh"; } # Dark Reader 
-      { id = "ldpochfccmkkmhdbclfhpagapcfdljkj"; } # Decentraleyes 
       { id = "fdpohaocaechififmbbbbbknoalclacl"; } # GoFullPage
-      { id = "bmnlcjabgnpnenekpadlanbbkooimhnj"; } # Honey 
       { id = "amknoiejhlmhancpahfcfcfhllgkpbld"; } # Hoppscotch Browser Extension
       { id = "blipmdconlkpinefehnmjammfjpmpbjk"; } # Lighthouse
       { id = "hniebljpgcogalllopnjokppmgbhaden"; } # Scre.io
       { id = "mooikfkahbdckldjjndioackbalphokd"; } # Selenium IDE
       { id = "fmkadmapgofadopljbjfkapdkoienihi"; } # React Developer Tools 
-      { id = "cjpalhdlnbpafiamejdnhcphjbkeiagm"; } # uBlock Origin 
-      { id = "ogfcmafjalglgifnmanfmnieipoejdcf"; } # uMatrix 
       { id = "dbepggeogbaibhgnhhndojpepiihcmeb"; } # Vimium 
       { id = "nhdogjmejiglipccpnnnanhbledajbpd"; } # Vue.js devtools 
       { id = "iaajmlceplecbljialhhkmedjlpdblhp"; } # Vue.js devtools (legacy) 
