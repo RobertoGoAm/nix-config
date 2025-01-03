5d4620740ac884a252ecedadddeec950cf609caf
Roberto Gomez <robertogoam@disroot.org>
2025-01-03 14:05:17 +0100

    feat: further added ssh support

--- home-manager/features/cli/default.nix
+++ home-manager/features/cli/default.nix
@@ -1,7 +1,9 @@
 {
   imports = [
     ./alacritty.nix
+    ./gpg.nix
     ./ripgrep.nix
+    ./ssh.nix
     ./starship.nix
     ./zsh.nix
   ];
--- home-manager/features/cli/ssh.nix
+++ home-manager/features/cli/ssh.nix
@@ -1,4 +1,24 @@
 {
+  services = {
+    fail2ban = {
+      enable = true;
+    };
+
+    openssh = {
+      enable = true;
+      settings = {
+        # Harden
+        PasswordAuthentication = false;
+        PermitRootLogin = "no";
+
+        # Automatically remove stale sockets
+        StreamLocalBindUnlink = "yes";
+
+        X11Forwarding = true;
+      };
+    };
+  };
+
   programs.ssh = {
     enable = true;
   };
