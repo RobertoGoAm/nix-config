15164425e96d7a3ba89262df8241ae17278d2afd
Roberto Gomez <robertogoam@disroot.org>
2025-01-03 18:02:48 +0100

    feat: added packages for vulcan

--- home-manager/vulcan.nix
+++ home-manager/vulcan.nix
@@ -23,6 +23,18 @@
     homeDirectory = "/Users/robertogoam";
     stateVersion = "24.11";
     username = "robertogoam";
+
+    packages = with pkgs; [
+      # Development
+      nixd
+      nixfmt-rfc-style
+
+      # Tool
+      nerd-fonts.jetbrains-mono
+
+      # Work
+      git-credential-manager
+    ];
   };
 
   programs.home-manager.enable = true;
