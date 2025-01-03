920310cd78732568634efa4f4bb9766bd3887dfc
Roberto Gomez <robertogoam@disroot.org>
2025-01-03 14:05:17 +0100

    feat: further added ssh support

---
+++ home-manager/features/cli/gpg.nix
@@ -0,0 +1,30 @@
+{
+  services.gpg-agent = {
+    enable = true;
+    enableSshSupport = true;
+  };
+
+  programs =
+    let
+      fixGpg =
+        /*
+      bash
+        */
+        ''
+          gpgconf --launch gpg-agent
+        '';
+    in
+    {
+      # Start gpg-agent if it's not running or tunneled in
+      # SSH does not start it automatically, so this is needed to avoid having to use a gpg command at startup
+      # https://www.gnupg.org/faq/whats-new-in-2.1.html#autostart
+      bash.profileExtra = fixGpg;
+      fish.loginShellInit = fixGpg;
+      zsh.loginExtra = fixGpg;
+      nushell.extraLogin = fixGpg;
+
+      gpg = {
+        enable = true;
+      };
+    };
+}
