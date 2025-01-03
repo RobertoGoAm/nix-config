80672495b835961092b54e63de969ff28cdefc78
Roberto Gomez Amores <robertogoam@disroot.org>
2025-01-03 14:16:27 +0100

    chore: removed unsupported services

--- home-manager/features/cli/ssh.nix
+++ home-manager/features/cli/ssh.nix
@@ -1,24 +1,4 @@
 {
-  services = {
-    fail2ban = {
-      enable = true;
-    };
-
-    openssh = {
-      enable = true;
-      settings = {
-        # Harden
-        PasswordAuthentication = false;
-        PermitRootLogin = "no";
-
-        # Automatically remove stale sockets
-        StreamLocalBindUnlink = "yes";
-
-        X11Forwarding = true;
-      };
-    };
-  };
-
   programs.ssh = {
     enable = true;
   };
