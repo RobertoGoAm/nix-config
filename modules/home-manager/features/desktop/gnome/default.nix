ee37198108f7ebaa97a1fcb48c6db122b5f17d9f
Roberto Gomez <robertogoam@disroot.org>
2025-01-05 00:33:44 +0100

    feat: initial tilling configurations

--- modules/home-manager/features/desktop/gnome/default.nix
+++ modules/home-manager/features/desktop/gnome/default.nix
@@ -11,5 +11,19 @@
         "sp-tray@sp-tray.esenliyim.github.com"
       ];
     };
+
+    "/org/gnome/shell/extensions/forge/" = {
+      keybindings = {
+        window-focus-up = [ "<Alt>e" ];
+        window-focus-left = [ "<Alt>h" ];
+        window-focus-right = [ "<Alt>i" ];
+        window-focus-down = [ "<Alt>n" ];
+
+        window-move-up = [ "<Alt><Shift>e" ];
+        window-move-left = [ "<Alt><Shift>h" ];
+        window-move-right = [ "<Alt><Shift>i" ];
+        window-move-down = [ "<Alt><Shift>n" ];
+      };
+    };
   };
 }
