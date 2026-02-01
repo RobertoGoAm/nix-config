f8d6505a34fb7f5165433a0a98f112b9cd40bd2a
Roberto Gomez Amores <RobertoGoAm@disroot.org>
2026-02-01 16:26:51 +0100

    refactor(vulcan): replicate apps migration and imports sync from prometheus

--- modules/home-manager/hosts/vulcan/packages.nix
+++ modules/home-manager/hosts/vulcan/packages.nix
@@ -6,17 +6,47 @@
 
   home.packages = with pkgs; [
     # Development
+    antigravity
     cabal-install
+    claude-code
+    chatgpt
+    code-cursor
+    cursor-cli
+    fnm
     ghc
+    gemini-cli
+    glab
     haskell-language-server
+    ngrok
     nixd
     nixfmt-rfc-style
+    postman
     stack
 
+    # Productivity
+    anki-bin
+    raycast
+
+    # Social
+    telegram-desktop
+
+    # Media
+    iina
+    spotify
+
     # Tool
+    ansible
+    coreutils
+    cyberduck
     nerd-fonts.jetbrains-mono
+    procps
+    qbittorrent
+    ripgrep
+    the-unarchiver
+    vlc-bin
 
     # Work
     git-credential-manager
+    openfortivpn
   ];
 }
