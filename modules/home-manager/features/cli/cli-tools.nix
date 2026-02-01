c510041f0a5ff0061e61e571adb7fd95f5289d64
Roberto Gomez Amores <RobertoGoAm@disroot.org>
2026-02-01 16:18:41 +0100

    refactor(config): migrate java/jq to modules and add more system defaults

---
+++ modules/home-manager/features/cli/cli-tools.nix
@@ -0,0 +1,37 @@
+{ pkgs, ... }:
+{
+  programs.direnv = {
+    enable = true;
+    nix-direnv.enable = true;
+  };
+
+  programs.fzf = {
+    enable = true;
+    enableZshIntegration = true;
+  };
+
+  programs.bat = {
+    enable = true;
+    config = {
+      theme = "TwoDark";
+    };
+  };
+
+  programs.eza = {
+    enable = true;
+    enableZshIntegration = true;
+    icons = "auto";
+  };
+
+  programs.zoxide = {
+    enable = true;
+    enableZshIntegration = true;
+  };
+
+  programs.jq.enable = true;
+
+  programs.java = {
+    enable = true;
+    package = pkgs.zulu;
+  };
+}
